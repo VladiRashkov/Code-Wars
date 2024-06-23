2f3cfdbaa807d369ac556a799f58a5a4de07ac94
Vladislav <vladislav.rashkoff@gmail.com>
2024-06-23 16:34:37 +0300

    finding_length_of_the_sequence

--- finding_length_of_the_sequence.py
+++ finding_length_of_the_sequence.py
@@ -29,3 +29,12 @@ numbers = list(map(int, input().split(',')))
 n = int(input())
 
 print(length_of_sequence(numbers, n))
+
+
+# As part of this kata, you need to find the length of the sub-array that begins and ends with the specified number.
+
+# For example, if the array arr is [0, -3, 7, 4, 0, 3, 7, 9], finding the length of the sub-array that begins and ends with 7s would return 5.
+
+# For sake of simplicity, there will only be numbers (positive or negative) in the supplied array.
+
+# If there are less or more than two occurrences of the number to search for, return 0.
