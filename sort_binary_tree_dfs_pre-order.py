2612e486eded7e9d6112cdb6ed964d9f31791aa7
Vladi Rashkov <vladislav.rashkoff@gmail.com>
2024-07-23 16:23:24 +0300

    modified sort binary tree

--- sort_binary_tree_dfs_pre-order.py
+++ sort_binary_tree_dfs_pre-order.py
@@ -1,29 +1,34 @@
 from collections import deque
 
-class Node:
-    def __init__(self, L, R, n):
+
+class Node():
+    def __init__(self, L, R, V):
         self.left = L
         self.right = R
-        self.value = n
-
+        self.value = V
+        
 def level_order_traversal(root):
     if root is None:
         return []
-
+    
     result = []
     queue = deque([root])
-
+    
+    
     while queue:
         node = queue.popleft()
         result.append(node.value)
         
         if node.left:
             queue.append(node.left)
+        
         if node.right:
             queue.append(node.right)
-    
+            
     return result
 
-root1 = Node(Node(None, None, 1), Node(None, None, 3), 8)
-root1 = Node(root1, Node(Node(None, None, 4), Node(None, None, 5), 9), 2)
-print(level_order_traversal(root1))  
+
+root1 = Node(Node(None,None,1),Node(None,None,3),8)
+root2 = Node(Node(None,None,9),Node(None, None,4),5)
+root3 = Node(root1,root2,2)
+print(level_order_traversal(root3))
