a24a95a9ad9ade65b46c206077733903a62d6f95
Vladi Rashkov <vladislav.rashkoff@gmail.com>
2024-07-31 18:24:06 +0300

    smaller_than_me

--- smaller_than_me.py
+++ smaller_than_me.py
@@ -1,17 +1,19 @@
 def smaller(arr):
-    new_arr = []
     
-    for i in range(len(arr)):
-        count=0
-        for j in range(i, len(arr)):
-            if arr[i] > arr[j]:
-                count+=1
-           
-        new_arr.append(count)
-              
+    sorted_unique_arr = sorted(set(arr))
+    rank_map = {num: rank for rank, num in enumerate(sorted_unique_arr)}
     
-    return new_arr
-        
+    # Create a list to store the counts of numbers
+    counts = [0] * len(arr)
+    
+    # Iterate from right to left in the array
+    result = []
+    for num in reversed(arr):
+        rank = rank_map[num]
+        result.append(sum(counts[:rank]))
+        counts[rank] += 1
+    
+    return result[::-1]
 
-
-print(smaller(list(map(int, input().split(',')))))
+print(smaller([5, 4, 3, 2, 1]))  
+print(smaller([1, 2, 0]))        
